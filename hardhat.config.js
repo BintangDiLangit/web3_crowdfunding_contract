4119e27c3d9b2134b62eaa97fa75cca060cc56e7
BintangDiLangit <supercellbanned1@gmail.com>
2024-11-30 22:26:47 +0700

    initial

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,38 +1,17 @@
-require("@matterlabs/hardhat-zksync-solc");
-
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
-  networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
-    },
-  },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: "https://rpc.ankr.com/eth_sepolia/db02395efbf8049d74216713348e122a9b91b72f079d1fdbcfb8d15e2df4c206",
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
